79cf823fa9dba08d964424dc578c84e919e74731
Paul Godard <paul.godard@outlook.fr>
2020-12-07 15:45:22 +0100

    trash

--- map-reducer.js
+++ map-reducer.js
@@ -1,5 +1,12 @@
 const xlsxFile = require('read-excel-file/node');
+let text = [];
 
-xlsxFile('./Big Data.xlsx').then((rows) => {
- console.log(rows)
-})
+const getData = async() => {
+  xlsxFile('./Big Data.xlsx').then((rows) => {
+    text.push(rows);
+  })  
+  return text;
+}
+
+
+ console.log(getData());
